4b33c250c0d6ffde5cda21de7c5f03cbb80d4dcd
praveenashokkumar <pak348@nyu.edu>
2019-08-05 13:41:56 -0400

    adding the San Diego Authentication for API

---
+++ credentials.py
@@ -0,0 +1,17 @@
+# Please populate this file with your credentials provided by your city.  
+credentials = {
+    "City": { # city key is for clients with multiple tenant. it is recommended to leave the structure as is.
+        "name" :"{yourCity}",
+        "uaa": "https://auth.aa.cityiq.io",
+        "metadata":"{yourMetaDataURL}",
+        "event":"{yourEventServiceURL}",
+        "websocket": "wss://{yourWebSocketURL}", 
+        "developer": "{yourClientID}:{yourClientSecret}",
+        "parking": "{yourParkingPredixZoneID}",
+        "environment": "{yourEnvironmentPredixZoneID}",
+        "pedestrian": "{yourPedestrianPredixZoneID}",
+        "traffic": "{yourTrafficPredixZoneID}",
+        "bicycle": "{yourBicyclePredixZoneID}",
+        "bbox": "{lat}:{long},{lat}:{long}"
+    }
+}
