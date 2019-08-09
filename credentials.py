d24f994257ca6464e333478665d5a29d30c3cc00
praveenashokkumar <pak348@nyu.edu>
2019-08-09 10:34:46 -0400

    Updating Creds

--- credentials.py
+++ credentials.py
@@ -1,17 +1,17 @@
 # Please populate this file with your credentials provided by your city.  
 credentials = {
     "City": { # city key is for clients with multiple tenant. it is recommended to leave the structure as is.
-        "name" :"{yourCity}",
+        "name" :"San Diego",
         "uaa": "https://auth.aa.cityiq.io",
-        "metadata":"{yourMetaDataURL}",
-        "event":"{yourEventServiceURL}",
-        "websocket": "wss://{yourWebSocketURL}", 
-        "developer": "{yourClientID}:{yourClientSecret}",
-        "parking": "{yourParkingPredixZoneID}",
-        "environment": "{yourEnvironmentPredixZoneID}",
-        "pedestrian": "{yourPedestrianPredixZoneID}",
-        "traffic": "{yourTrafficPredixZoneID}",
-        "bicycle": "{yourBicyclePredixZoneID}",
+        "metadata":"https://sandiego.cityiq.io/api/v2/metadata/",
+        "event":"https://sandiego.cityiq.io/api/v2/event/",
+        "websocket": "wss://https://sandiego.cityiq.io/api/v2/websocket", 
+        "developer": "PublicAccess:uVeeMuiue4k=",
+        "parking": "SD-IE-PARKING",
+        "environment": "SD-IE-ENVIRONMENTAL",
+        "pedestrian": "SD-IE-PEDESTRIAN",
+        "traffic": "SD-IE-TRAFFIC",
+        "bicycle": "SD-IE-BIKE", 
         "bbox": "{lat}:{long},{lat}:{long}"
     }
 }
